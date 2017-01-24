7a424e84e5cd8e06d1da8cf050bb2579db9b6c1b
Vigneshwaran K <vigneshclassic97@gmail.com>
2017-01-24 11:29:01 +0530

    Admin - session changes

--- admin_login/jobs/job_filter.php
+++ admin_login/jobs/job_filter.php
@@ -1,6 +1,14 @@
 <?php  session_start();
 ob_start();
 
+if (!isset($_SESSION['user']) && $_SESSION['user'] == null && isset($_SESSION['user_role'])!='admin' ) {
+
+    header("Location: ../../login.html");
+
+
+}
+
+
 ?>
 
 
